c7146375d9145a52a634d75b7b97119cde8e185b
Pradheep <padhupradheep@gmail.com>
2024-10-28 16:49:54 +0100

    updating EMROX for caroline

--- rox_description/launch/description_launch.py
+++ rox_description/launch/description_launch.py
@@ -14,10 +14,11 @@ import os
 from pathlib import Path
 import xacro
 
-def execution_stage(context: LaunchContext, frame_type, rox_type, arm_type):
+def execution_stage(context: LaunchContext, frame_type, rox_type, arm_type, ur_dc):
     frame_typ = frame_type.perform(context)
     arm_typ = arm_type.perform(context)
     rox_typ = rox_type.perform(context)
+    use_ur_dc = ur_dc.perform(context)
 
     if (rox_typ == "meca"):
         frame_typ = "long"
@@ -36,15 +37,51 @@ def execution_stage(context: LaunchContext, frame_type, rox_type, arm_type):
             " ", 'arm_type:=',
             arm_typ,
             " ", 'rox_type:=',
-            rox_type])}],
+            rox_type,
+            " ", 'use_ur_dc:=',
+            use_ur_dc])}],
         arguments=[urdf])
 
     return [start_robot_state_publisher_cmd]
 
 def generate_launch_description():
-    opq_function = OpaqueFunction(function=execution_stage,
-                                  args=[LaunchConfiguration('frame_type',  default="short"),
-                                        LaunchConfiguration('rox_type',  default="argo"),
-                                        LaunchConfiguration('arm_type',  default="")])
+    
+    # Launch configuration
+    frame_type = LaunchConfiguration('frame_type')
+    rox_type = LaunchConfiguration('rox_type')
+    arm_type = LaunchConfiguration('arm_type')
+    ur_dc = LaunchConfiguration('use_ur_dc')
+   
+    declare_frame_type_cmd = DeclareLaunchArgument(
+            'frame_type', default_value='short',
+            description='Frame type - Options: short/long'
+        )
+    
+    declare_rox_type_cmd = DeclareLaunchArgument(
+            'rox_type', default_value='argo',
+            description='Robot type - Options: argo/diff/trike/meca'
+        )
+   
+    declare_arm_cmd = DeclareLaunchArgument(
+            'arm_type', default_value='',
+            description='Arm used in the robot - currently only support universal'
+        )
+    
+    declare_ur_pwr_variant_cmd = DeclareLaunchArgument(
+            'use_ur_dc', default_value='false',
+            description='Set this argument to True if you have an UR arm with DC variant'
+        )
 
-    return LaunchDescription([opq_function])
+    context_arguments = [frame_type, rox_type, arm_type, ur_dc]
+
+    opq_function = OpaqueFunction(function=execution_stage,
+                                  args=context_arguments)
+
+    ld = LaunchDescription()
+    ld.add_action(declare_rox_type_cmd)
+    ld.add_action(declare_arm_cmd)
+    ld.add_action(declare_frame_type_cmd)
+    ld.add_action(declare_ur_pwr_variant_cmd)
+    ld.add_action(opq_function)
+
+    return ld
